6abce99a6e02cecd1ad42b66d25323e893f2c5bf
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2021-04-12 16:52:40 +0530

    Added simple state and button

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState(
+    'Open up App.js to start working on your app!'
+  );
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{text}</Text>
+      <Button title="Click Here" onPress={() => setText('Button clicked!')} />
       <StatusBar style="auto" />
     </View>
   );
