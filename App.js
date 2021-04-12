8587841319a505625a7b35d0e73307f5ef47acfd
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2021-04-12 17:58:30 +0530

    Using inline styles and flexbox

--- App.js
+++ App.js
@@ -1,25 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   const [text, setText] = useState(
     'Open up App.js to start working on your app!'
   );
   return (
-    <View style={styles.container}>
-      <Text>{text}</Text>
-      <Button title="Click Here" onPress={() => setText('Button clicked!')} />
-      <StatusBar style="auto" />
+    <View style={{ padding: 50 }}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <TextInput
+          placeholder="Course Goal"
+          style={{
+            borderColor: '#f2f3f4',
+            borderWidth: 2,
+            borderRadius: 5,
+            width: '80%',
+            padding: 4,
+          }}
+        />
+        <Button title="ADD" style={{ width: '20%' }} />
+      </View>
+      <View></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
