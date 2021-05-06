e72e245fc01ad05c041e489bdaafbe777e8511cc
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2021-05-06 20:51:48 +0530

    Implemented local image in GameOverScreen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
 import colors from '../constants/colors';
 import defaultStyles from '../constants/defaultStyles';
 
 const GameOverScreen = ({ rounds, userNumber, onNewGame }) => {
   return (
     <View style={styles.screen}>
-      <Text style={defaultStyles.body}>GAME OVER</Text>
+      <Text style={defaultStyles.header}>GAME OVER</Text>
+      <View style={styles.imgContainer}>
+        <Image style={styles.img} source={require('../assets/success.png')} />
+      </View>
       <Text style={defaultStyles.body}>No. of Rounds: {rounds}</Text>
       <Text style={defaultStyles.body}>The Number: {userNumber}</Text>
       <Button title="New Game" onPress={onNewGame} />
@@ -23,5 +26,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: colors.light,
+  },
+  imgContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderColor: 'black',
+    borderWidth: 2,
+    overflow: 'hidden',
+    marginVertical: 12,
+  },
+  img: {
+    width: '100%',
+    height: '100%',
   },
 });
