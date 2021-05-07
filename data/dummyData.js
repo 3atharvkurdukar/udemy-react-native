eeb451ce5aee100a461c7086c96e3c43cb63fcbf
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2021-05-08 00:30:01 +0530

    Added dummy data grid

---
+++ data/dummyData.js
@@ -0,0 +1,14 @@
+import Category from '../models/Category';
+
+export const CATEGORIES = [
+  new Category('c1', 'Fast Food', '#f5428d'),
+  new Category('c2', 'Chinese', '#f54242'),
+  new Category('c3', 'Breakfast', '#f5a442'),
+  new Category('c4', 'Starters', '#f5d142'),
+  new Category('c5', 'Salads', '#368dff'),
+  new Category('c6', 'Main Course', '#41d95d'),
+  new Category('c7', 'Desserts', '#9eecff'),
+  new Category('c8', 'Specials', '#b9ffb0'),
+  new Category('c9', 'Beverages', '#ffc7ff'),
+  new Category('c10', 'Deals & Discounts', '#47fced'),
+];
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import { CATEGORIES } from '../data/dummyData';
 
-const CategoriesScreen = ({ navigation }) => {
+const renderGridItem = (itemData) => {
   return (
-    <View style={styles.screen}>
-      <Text>Categories Screen</Text>
-      <Button
-        title="Go to Meals"
-        onPress={() => navigation.navigate('CategoryMeals')} // Pushes the given screen into the stack
-        // onPress={() => navigation.replace('CategoryMeals')} // Replaces current screen with given
-      />
+    <View style={styles.gridItem}>
+      <Text style={{ ...styles.gridItemTitle, color: itemData.item.color }}>
+        {itemData.item.title}
+      </Text>
     </View>
+  );
+};
+
+const CategoriesScreen = () => {
+  return (
+    <FlatList numColumns={2} data={CATEGORIES} renderItem={renderGridItem} />
   );
 };
 
@@ -21,5 +26,23 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    borderRadius: 12,
+    shadowColor: 'black',
+    shadowOpacity: 0.24,
+    shadowOffset: { width: 0, height: 4 },
+    shadowRadius: 8,
+    elevation: 8,
+  },
+  gridItemTitle: {
+    fontFamily: 'InterBold',
+    fontSize: 22,
   },
 });
