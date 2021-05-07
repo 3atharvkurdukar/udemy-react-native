23cfb064202f1e0681148d5a686d307fcfaa067c
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2021-05-07 23:03:49 +0530

    Added react-navigation 4

--- App.js
+++ App.js
@@ -1,22 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
 
 export default function App() {
-  const [text, setText] = useState(
-    'Open up App.js to start working on your app!'
-  );
-  return (
-    <View style={styles.container}>
-      <Text>{text}</Text>
-      <Button title="Click Here" onPress={() => setText('Button clicked!')} />
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  const fetchFonts = () => {
+    return Font.loadAsync({
+      InterBlack: require('./assets/fonts/Inter-Black.ttf'),
+      InterRegular: require('./assets/fonts/Inter-Regular.ttf'),
+      InterSemiBold: require('./assets/fonts/Inter-SemiBold.ttf'),
+      InterBold: require('./assets/fonts/Inter-Bold.ttf'),
+    });
+  };
+
+  if (!fontsLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontsLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+
+  return <View style={styles.screen}></View>;
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
