1967ab39e9b44476579ea099b41b047e89312c7b
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2021-05-08 21:24:49 +0530

    Added bottom tabs navigator with icons Added Material Bottom Tab Navigator for android

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,10 +1,14 @@
-import { Platform } from 'react-native';
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 import colors from '../constants/colors';
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailsScreen from '../screens/MealDetailsScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
 
 const MealsNavigator = createStackNavigator(
   {
@@ -34,4 +38,42 @@ const MealsNavigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(MealsNavigator);
+const tabScreenConfig = {
+  Meals: {
+    screen: MealsNavigator,
+    navigationOptions: {
+      tabBarIcon: (tabInfo) => (
+        <Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />
+      ),
+    },
+  },
+  Favorites: {
+    screen: FavoritesScreen,
+    navigationOptions: {
+      tabBarIcon: (tabInfo) => (
+        <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />
+      ),
+    },
+  },
+};
+
+const MealsFavTabNavigator =
+  Platform.OS === 'android'
+    ? createMaterialBottomTabNavigator(tabScreenConfig, {
+        barStyle: {
+          backgroundColor: colors.background,
+        },
+        activeColor: colors.primary,
+        inactiveColor: 'grey',
+        shifting: true,
+      })
+    : createBottomTabNavigator(tabScreenConfig, {
+        tabBarOptions: {
+          activeBackgroundColor: colors.background,
+          inactiveBackgroundColor: colors.background,
+          activeTintColor: colors.primary,
+          inactiveTintColor: 'grey',
+        },
+      });
+
+export default createAppContainer(MealsFavTabNavigator);
