c191feba2cf6f65403765776a7127b65a856e2cc
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2021-05-15 16:20:32 +0530

    Added color scheme and default styles

---
+++ constants/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+  primary: '#FF7465',
+  accent: '#FC3637',
+  background: '#FFFFFF',
+  lightGrey: '#D8D8D8',
+  grey: '#B8B8B9',
+  darkText: '#001833',
+};
+
+export default colors;
---
+++ constants/defaultStyles.js
@@ -0,0 +1,9 @@
+const defaultStyles = {
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+  },
+};
+
+export default defaultStyles;
