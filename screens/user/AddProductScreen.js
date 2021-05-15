ec2fb0be540c057f456f83a866d074852293885c
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2021-05-15 15:24:28 +0530

    Added necessary screens and packages

---
+++ screens/user/AddProductScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const AddProductScreen = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default AddProductScreen;
+
+const styles = StyleSheet.create({});
