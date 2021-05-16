ca3dba0165883ab865703beca3ed745b0bfdc2fe
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2021-05-17 00:28:45 +0530

    Created header button to add cart icon to top

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,6 +1,7 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import colors from '../constants/colors';
+import CartScreen from '../screens/shop/CartScreen';
 import ProductDetailsScreen from '../screens/shop/ProductDetailsScreen';
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
 
@@ -8,6 +9,7 @@ const ProductsNavigator = createStackNavigator(
   {
     ProductsOverview: ProductsOverviewScreen,
     ProductDetails: ProductDetailsScreen,
+    Cart: CartScreen,
   },
   {
     defaultNavigationOptions: {
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import ProductItem from '../../components/ProductItem';
 import defaultStyles from '../../constants/defaultStyles';
+import { addToCart } from '../../store/actions/cart';
+import CustomHeaderButton from '../../components/HeaderButton';
 
 const ProductsOverviewScreen = ({ navigation }) => {
   const products = useSelector((state) => state.products.availableProducts);
+  const dispatch = useDispatch();
 
   return (
     <View style={defaultStyles.screen}>
@@ -22,6 +26,9 @@ const ProductsOverviewScreen = ({ navigation }) => {
                 prodTitle: itemData.item.title,
               });
             }}
+            onAddToCart={() => {
+              dispatch(addToCart(itemData.item.id, 1));
+            }}
           />
         )}
       />
@@ -34,8 +41,21 @@ const styles = StyleSheet.create({
   },
 });
 
-ProductsOverviewScreen.navigationOptions = {
-  headerTitle: 'All Products',
+ProductsOverviewScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerTitle: 'All Products',
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          title="Favorite"
+          iconName="cart"
+          onPress={() => {
+            navigation.navigate('Cart');
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
 };
 
 export default ProductsOverviewScreen;
